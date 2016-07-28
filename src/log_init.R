68ed907d7954ed69cc3163ac8ce69f4aa2e5365a
Joshua Slocum <joshuaslocum@gmail.com>
2016-07-27 23:19:51 -0700

    Created a function that creates a model-log file in the specified directory if one does not exist

---
+++ src/log_init.R
@@ -0,0 +1,37 @@
+# Header ------------------------------------------------------------------
+# Created: 7/27/2016
+# Author: Joshua Slocum
+# Purpose: simple function to create model log file
+
+
+# Define Function ---------------------------------------------------------
+
+log_init <- function(path = getwd()) {
+  
+  # Set full path with file name
+  log_file <- paste(path, "model-log.txt", sep = "/")
+  
+  # Check if model-log.txt already exists
+  # Stop if it exists
+  file_exists <- file.exists(log_file)
+  if (file_exists) {stop("model-log.txt already exists in directory.")}
+  
+  # Create log file, it will be tab separated
+  # By convention each line will end with a newline
+  file.create(log_file)
+  
+  # Create log file header lind
+  log_header <- paste(c("model_type", "specificaiton", "data")
+                      ,collapse = "\t")
+  
+  # Add header to file
+  cat(log_header
+      ,file = log_file
+      ,sep = ""
+      ,fill = T
+      ,append = TRUE)
+  
+  # Show message that file was created to console
+  message(paste("Created model-log.txt in", path, sep = " "))
+}
+

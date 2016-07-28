bf5a839d07d37556872f32fdf8f5c0aa1b3ed211
JoshuaSlocum <JoshuaSlocum@gmail.com>
2016-07-27 22:01:22 -0700

    Started work on simple model log workflow. Sketched it out in a simple script with text file for log.

---
+++ src/model-log-workflow.R
@@ -0,0 +1,31 @@
+# Header ------------------------------------------------------------------
+# Created: 7/27/2016
+# Author: Joshua Slocum
+# Purpose: Example of model log workflow
+
+# To start we will simply log basic info line by line
+
+# Create log file, it will be tab separated 
+file.create("model-log.txt")
+
+# Create header for file
+log_header <- c("model_type", "specificaiton", "data")
+cat(paste(log_header, collapse = "\t")
+    ,file = "model-log.txt"
+    ,sep = ""
+    ,fill = T
+    ,append = TRUE)
+
+# Load data and fit fake model
+data("mtcars")
+model_1  <- lm(mpg ~ as.factor(cyl) + hp + wt, data = mtcars)
+
+# Write model call to file
+cat(paste(as.character(model_1$call), collapse = "\t")
+    ,file = "model-log.txt"
+    ,append = TRUE
+    ,sep = ""
+    ,fill = T)
+
+# Test of reading log file in to DF
+model_log <- read.delim("model-log.txt", )

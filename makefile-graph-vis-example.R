fc46378486e5e1b47b55220230ad902e2c7c8989
JoshuaSlocum <JoshuaSlocum@gmail.com>
2016-10-17 22:10:54 -0700

    added starter outline to making a diagram of a makefile

---
+++ makefile-graph-vis-example.R
@@ -0,0 +1,13 @@
+# Header ------------------------------------------------------------------
+# Created: 10/17/2016
+# Author: Joshua Slocum
+# Purpose: Plot a DAG of a Makefile
+
+# How to do:
+# 1) figure out how to read file in
+# 2) figure out how to parse structure
+# 3) turn structure into thing for diagrammer
+# 4) do manually 
+# 5) functionize
+
+library(DiagrammeR)

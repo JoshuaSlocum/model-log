fa4d4cd4d1ecd21a86857fa2a57cd4962f8707c0
Joshua Slocum <joshuaslocum@gmail.com>
2017-01-23 22:25:41 -0800

    Added a test for log_add

--- tests/testthat/test-log_add.R
+++ tests/testthat/test-log_add.R
@@ -1,2 +1,14 @@
 library(model.log)
 context("Model Object Entry to Log")
+
+# GLM Method Tests --------------------------------------------------------
+test_that("Success Message is displayed",{
+  log_init()
+  data("mtcars")
+  model_glm  <- glm(mpg ~ as.factor(cyl) + hp + wt, data = mtcars)
+  expect_message(log_add(model_glm, "model_log.csv"), "Added new model entry to model log")
+})
+
+
+
+file.remove("model_log.csv")
